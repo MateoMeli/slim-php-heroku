2c3a8b2bc4d00e91f8c739c372c56855f02147d6
Mateo Meli <mateomeli7@gmail.com>
2021-03-22 19:38:35 -0300

    first commit

--- app/index.php
+++ app/index.php
@@ -11,7 +11,7 @@ $app = new \Slim\App([]);
 
 
 $app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
+    $response->getBody()->write("Ahora anda lo que estas haciendo");
     return $response;
 
 });
