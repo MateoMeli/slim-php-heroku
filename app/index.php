634c78018b4d054935e2961e078ff12ebc305645
mateomeli <mateomeli7@gmai.com>
2021-03-25 09:48:08 -0300

    Probando html

--- app/index.php
+++ app/index.php
@@ -1,20 +1,78 @@
-<?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Documentos</title>
+    <style>
+        form{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <!-- Label -->
+    <!-- Input -->
+    <!-- Seleccion -->
+    <!-- Button -->
+    <h1>Formularios</h1>
+    <form method="GET">
+        <label for="txtNombre">Nombre:</label>
+        <input type="text" id="txtNombre" name="nombre" minlength="2" autocomplete="off"><br>
+        <label for="txtEdad">Edad:</label>
+        <input type="number" min="18" max="65" id="txtEdad" name="edad" autocomplete="off"><br>
+        <label for="txtClave">Clave:</label>
+        <input type="password" id="txtClave" name="clave" autocomplete="off"><br>
+        <label for="txtEmail">Email:</label>
+        <input type="email" id="txtEmail" name="email" autocomplete="off"><br>
+        
+        <fieldset>
+            <legend>Genero</legend>
+            <input type="radio" name="sexo" id="" value="M">Masculino<br>
+            <input type="radio" name="sexo" id="" value="F" checked>Femenino<br>
+        </fieldset>
+        <fieldset>
+            <legend>Deportes</legend>
+            <input type="checkbox" name="deporte" value="N">Natacion<br>
+            <input type="checkbox" name="deporte" value="C">Ciclismo<br>
+            <input type="checkbox" name="deporte" value="F">Futbol<br>
+        </fieldset>
 
-require_once '../vendor/autoload.php';
+        <label for="cboLocalidad">Localidad</label>
+        <select name="localidad" id="cboLocalidad">
+            <optgroup label="Zona sur">
+            <option value="Avellaneda">Avellaneda</option>
+            <option value="Banfield">Banfield</option>
+            <option value="Temperley">Temperley</option>
+            <option value="La matanza">La matanza</option>
+            <option value="Quilmes">Quilmes</option>
+        </optgroup>
+        <optgroup label="Zona oeste">
+            <option value="Lomas">Lomas</option>
+            <option value="Gerli">Gerli</option>
+            <option value="Banfield">Banfield</option>
+            <option value="Burzaco">Burzaco</option>
+            <option value="Caseros">Caseros</option>
+        </optgroup>
+        </select> <br><br><br>
+        
+        <label for="">Lenguajes de programacion</label>
+        <input type="list" list="Lenguajes">
+        <datalist id="Lenguajes">
+            <option value="C#">C#</option>
+            <option value="Java">Java</option>
+            <option value="PHP">PHP</option>
+            <option value="JS">JS</option>
+            <option value="Kotlin">Kotlin</option>
+        </datalist> <br><br><br>
 
+        <img src="./assets/images/IconoUtn.png" alt=""> <br><br><br>
 
+        <input type="date" name="" id="">
 
-$app = new \Slim\App([]);
-
-
-
-$app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("Mateo Meli");
-    return $response;
-
-});
-
-
-$app->run();
+        <input type="reset">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
