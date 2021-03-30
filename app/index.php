86da078cd73ca508366fb60ad315cce551f35f65
Mateo Meli <mateomeli7@gmail.com>
2021-03-30 20:53:23 -0300

    prueba labo

--- app/index.php
+++ app/index.php
@@ -1,78 +1,57 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Documentos</title>
-    <style>
-        form{
-            color: red;
-        }
-    </style>
+    <title>Document</title>
 </head>
 <body>
-    <!-- Label -->
-    <!-- Input -->
-    <!-- Seleccion -->
-    <!-- Button -->
-    <h1>Formularios</h1>
-    <form method="GET">
-        <label for="txtNombre">Nombre:</label>
-        <input type="text" id="txtNombre" name="nombre" minlength="2" autocomplete="off"><br>
-        <label for="txtEdad">Edad:</label>
-        <input type="number" min="18" max="65" id="txtEdad" name="edad" autocomplete="off"><br>
-        <label for="txtClave">Clave:</label>
-        <input type="password" id="txtClave" name="clave" autocomplete="off"><br>
-        <label for="txtEmail">Email:</label>
-        <input type="email" id="txtEmail" name="email" autocomplete="off"><br>
-        
-        <fieldset>
-            <legend>Genero</legend>
-            <input type="radio" name="sexo" id="" value="M">Masculino<br>
-            <input type="radio" name="sexo" id="" value="F" checked>Femenino<br>
-        </fieldset>
-        <fieldset>
-            <legend>Deportes</legend>
-            <input type="checkbox" name="deporte" value="N">Natacion<br>
-            <input type="checkbox" name="deporte" value="C">Ciclismo<br>
-            <input type="checkbox" name="deporte" value="F">Futbol<br>
-        </fieldset>
+    <h1>Venta de casas y cepartamentos exclusivos de cujo</h1>    
 
-        <label for="cboLocalidad">Localidad</label>
-        <select name="localidad" id="cboLocalidad">
-            <optgroup label="Zona sur">
-            <option value="Avellaneda">Avellaneda</option>
-            <option value="Banfield">Banfield</option>
-            <option value="Temperley">Temperley</option>
-            <option value="La matanza">La matanza</option>
-            <option value="Quilmes">Quilmes</option>
-        </optgroup>
-        <optgroup label="Zona oeste">
-            <option value="Lomas">Lomas</option>
-            <option value="Gerli">Gerli</option>
-            <option value="Banfield">Banfield</option>
-            <option value="Burzaco">Burzaco</option>
-            <option value="Caseros">Caseros</option>
-        </optgroup>
-        </select> <br><br><br>
-        
-        <label for="">Lenguajes de programacion</label>
-        <input type="list" list="Lenguajes">
-        <datalist id="Lenguajes">
-            <option value="C#">C#</option>
-            <option value="Java">Java</option>
-            <option value="PHP">PHP</option>
-            <option value="JS">JS</option>
-            <option value="Kotlin">Kotlin</option>
-        </datalist> <br><br><br>
+    <h2>Mas sobre nosotros</h2>
 
-        <img src="./assets/images/IconoUtn.png" alt=""> <br><br><br>
+    <h3>Seguridad</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto quaerat et adipisci excepturi eligendi fugit veritatis est, veniam temporibus praesentium dolor eveniet enim maxime quae quisquam necessitatibus. Pariatur, odio repellat.</p>
+    <h3>El mejor precio</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit ducimus labore voluptatum expedita debitis alias aliquid, voluptate doloribus deserunt pariatur omnis iste ipsa ab eos ut porro inventore adipisci repellendus!</p>
+    <h3>A tiempo</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae dolore non totam, numquam ut tenetur inventore in quia officia, ratione hic. Illo est aliquid, necessitatibus cum corrupti in modi voluptates?</p>
 
-        <input type="date" name="" id="">
+    <h2>Casas y departamentos en venta</h2>
+    <h3>Casa de lujo en el lago</h3>
+    <p>Casa en el lago con excelente vista a un excelente precio</p>
+    <p>U$S350.000</p>
+    <h3>Casa terminados de lujo</h3>
+    <p>Casa con diseño modernos asi como tecnologia inteligente y amueblada</p>
+    <p>U$S275.000</p>
+    <h3>Casa con pileta</h3>
+    <p>Casa con pileta y acabados de lujo en la ciudad. excelente oportunidad</p>
+    <p>U$S250.000</p>
 
-        <input type="reset">
-        <button type="submit">Enviar</button>
-    </form>
+    <h2>Encuentra la casa de tus sueños</h2>
+    <p>Llena el formulario de contacto y un ascesor se pondra en contacto a la brevedad</p>
+
+    <h2>Nuestro blog</h2>
+    <h3>Terraza en el techo de tu casa</h3>
+    <p>Escrito el: <date>30/03/2021</date> por: <span>Admin</span></p>
+    <p>Consejos para construir una terraza en el techo de tu casa con los mejores materiales y ahorrando mucho dinero</p>
+    <h3>Guia para la decoracion de tu hogar</h3>
+    <p>Escrito el: <date>29/03/2021</date> por: <span>Admin</span></p>
+    <p>Maximiza el espacio de tu hogar con esta guia. aprende a combinar los muebles y colores para darle vida a tu espacio</p>
+
+    <h2>Comentarios</h2>
+    <blockquote>
+        El personal se comporto de una excelente forma, muy buena atencion y la casa que me ofrecieron cumple con todas mis expectativas.
+        <cite>-Juan Perez</cite>
+    </blockquote>
+    <p>Todos los derechos reservados <date>2021</date> &copy; </p>
+    <nav>
+        <ul>
+            <li>Nosotros</li>
+            <li>Anuncios</li>
+            <li>Blog</li>
+            <li>Contactos</li>
+        </ul>
+    </nav>
 </body>
 </html>
